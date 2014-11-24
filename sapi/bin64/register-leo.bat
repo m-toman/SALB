b48c2caf8b9bb1fb91744745e87269b3185cd71a
Markus Toman <m.toman@neuratec.com>
2014-11-24 17:57:23 +0100

    Batch files for registering SAPI
    Example batch files for registering SAPI voice model

---
+++ sapi/bin64/register-leo.bat
@@ -0,0 +1,39 @@
+@echo off
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "%~s0", "%params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+
+
+cd %~dp0
+echo %CD%
+
+@echo on
+
+regsvr32 "%CD%\htstts.dll"
+"%CD%\register-voice.exe" "%CD%\leo.htsvoice" "Leopold64" "de-at" "male" "adult" "%CD%\leo.log" "%CD%\leo.rules"
+
+@echo off
+
+pause
